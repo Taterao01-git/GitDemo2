8dba9b8b33f40b87e3761f51ac69f483e57f800a
shitaladmane444 <shitaladmane444@gmail.com>
2025-01-11 22:28:37 +0530

    added files

--- src/test/java/Pages/Demo4.java
+++ src/test/java/Pages/Demo4.java
@@ -5,7 +5,7 @@ public class Demo4 {
 	public static void main(String[] args)
 	{
 		System.out.println("Demo4 created by Taterao");
-		System.out.println("Demo4 created by Taterao is Suraj");
+
 	}
 
 }
---
+++ src/test/java/Pages/Demo6.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class Demo6 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Demo6");
+    }
+}
