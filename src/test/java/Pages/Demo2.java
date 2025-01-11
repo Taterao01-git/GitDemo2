eb50c071eb7037ff32d614ff12e76f9c73edf484
Taterao01-git <tateraog63@gmail.com>
2025-01-11 20:42:24 +0530

    c3

--- src/test/java/Pages/Demo2.java
+++ src/test/java/Pages/Demo2.java
@@ -5,6 +5,8 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("This is demo2");
 		System.out.println("This is demo3");
+		System.out.println("This is demo 4");
+		
 	}
 
 }
