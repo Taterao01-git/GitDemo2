705487fe9d1d1fb706a7623a31f1f2df2e62a1b9
Taterao01-git <tateraog63@gmail.com>
2025-01-13 00:39:41 +0530

    Update Demo12.java

--- src/test/java/Pages/Demo12.java
+++ src/test/java/Pages/Demo12.java
@@ -2,6 +2,6 @@ public class Demo12 {
 
     public static void main(String[] args)
     {
-        System.out.println("Demo12 created by shital");
+        System.out.println("Demo12 created by Taterao");
     }
 }
