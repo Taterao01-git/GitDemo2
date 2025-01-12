8d1c8787405a7103e0f7c9c003467bcc4fa259e7
shitaladmane444 <shitaladmane444@gmail.com>
2025-01-12 23:32:44 +0530

    Hello

---
+++ src/test/java/Pages/Demo6.java
@@ -0,0 +1,25 @@
+package Pages;
+
+public class Demo6 {
+
+	public static void main(String[] args) {
+		
+		//-98+97=
+//		int g = -98;
+//		int h = g++;
+//
+//		System.out.println(g);//-98
+//		System.out.println(h);
+		
+		//Pre increment
+		
+		int a=1;
+		int b=++a;
+		
+		System.out.println(a);//2
+		System.out.println(b);//2
+		
+		
+	}
+
+}
