b59aef3663001d374f08a3eb476f0cb6b74e0ca2
shitaladmane444 <shitaladmane444@gmail.com>
2025-01-13 01:29:53 +0530

    Demo14 is updated

---
+++ src/test/java/Pages/Demo14.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class Demo14 {
+
+    public static void main(String[] args) {
+        String name="Latur";
+        System.out.println("Demo14 file created by shital");
+    }
+}
